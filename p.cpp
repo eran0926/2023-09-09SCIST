15f20e5deb30140431bb5fb53798baf384ee8779
eran0926 <eran0926@gmail.com>
2023-09-16 00:43:18 +0800

    update p

--- p.cpp
+++ p.cpp
@@ -1,35 +1,54 @@
 #include<iostream>
 
 using namespace std;
-int arr[1000000];
+long long r_arr[1000000];
+long long arr[1000000];
+long long pre_arr[1000000];
+int n, k;
+long long score = 0;
+int s = 0, e = 0, sum = 0;
+// int s_max_id = 0
+int s_max = 0;
 int main(){
-	int n, k;
-	int score = 0;
 	cin >> n;
 	cin >> k;
 	for (int i = 0; i<n; i++){
-		cin >> arr[i];
+		cin >> r_arr[i];
 	}
-	int all = 0;
-	for (int i = 0; i<n; i++){
-		all += arr[i];
-	}
-	score += all*(k/n);
 	
-	int s = 0;
-	// int s_max_id = 0
-	int s_max = 0;
-	// cout << k%n << "a\n";
-	for (int i = 0; i<n-(k%n); i++){
-		 s = 0;
-		 for (int j = i; j<(i+(k%n)); j++){
-		 	s += arr[j];
-		 }
-		 if (s>s_max){
-		 	s_max = s;
-		 	// s_max_id = j;
-		 }
+	for (int i = 0; i<n; i++){
+	    if (r_arr[i]-(k/n) < 0){
+	        score += r_arr[i]*(r_arr[i]+1)/2;
+	        arr[i] = 0;
+	    }
+	    else {
+	        score += (k/n)*(r_arr[i] + r_arr[i]-k/n+1)/2;
+	        arr[i] = r_arr[i]-(k/n);
+	    }
 	}
+	pre_arr[0] = 0;
+	// cout << k%n << "a\n";
+	for (int i = 1; i<=n; i++){
+		 pre_arr[i] = pre_arr[i-1] + arr[i-1];
+	}
+		 
+	for (int i = 1; i<=n; i++){
+	    s = i - 1;
+	    e = i + k%n - 1;
+	    sum = 0;
+	    if (e > n){
+	        sum += pre_arr[n] - pre_arr[s];
+	        sum += pre_arr[e%n] - pre_arr[0];
+	    }
+	    else{
+	        sum += pre_arr[e] - pre_arr[s];
+	    }
+	    if(sum > s_max){
+	        s_max = sum;
+	    }
+	}
+	//cout << score <<endl;
+	//cout << s_max <<endl; 
 	score += s_max;
 	cout << score;
 }
